fe1186a8d1c9424a6be6ebb855111fae725aa22c
Vincent Mwinzila <vincentmwinzila.5@gmail.com>
2019-11-24 20:05:13 +0300

    Created the countdown timer elements for the Questions Module

--- test.php
+++ test.php
@@ -21,6 +21,8 @@
             </div>        
           </div>
         </div>
+        nzomo254 
+
 
         <div class="content-box-large">
         <div class="panel-body">  
@@ -66,6 +68,40 @@
 
                                   }
                                  ?>
+
+                                // <!-- Display the countdown timer in an element -->
+                                <p id="demo"></p>
+
+                                <script>
+                                // Set the date we're counting down to
+                                var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+
+                                // Update the count down every 1 second
+                                var x = setInterval(function() {
+
+                                  // Get today's date and time
+                                  var now = new Date().getTime();
+
+                                  // Find the distance between now and the count down date
+                                  var distance = countDownDate - now;
+
+                                  // Time calculations for days, hours, minutes and seconds
+                                  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                                  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                                  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                                  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                                  // Display the result in the element with id="demo"
+                                  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+                                  + minutes + "m " + seconds + "s ";
+
+                                  // If the count down is finished, write some text
+                                  if (distance < 0) {
+                                    clearInterval(x);
+                                    document.getElementById("demo").innerHTML = "EXPIRED";
+                                  }
+                                }, 1000);
+                                </script>
         <form class="form-horizontal" role="form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <input type="hidden"class="form-control" id="question_id" name="question_id" value="32" />
         <div class="form-group">
